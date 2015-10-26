5e0bad0c3b0268e9b16b1a63dff32b0ade040815
Matt Morrisey <mattydevicce@gmail.com>
2015-10-26 15:45:10 -0400

    Added model validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   belongs_to :gym
+  validates :email, uniqueness: true
+  validates :name, :email, :password_confirmation, presence: true
+  validates :password, length: { minimum: 6 }
 end
